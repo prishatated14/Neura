07fa54029fae66e90020800ee53e1a4a5cdc7dec
prishatated14 <prishatated14@gmail.com>
2025-03-15 22:22:04 +0530

    `Added eel integration and Microsoft Edge app mode launch in main.py, updated www/index.html with favicon and eel.js script inclusion.`

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound  
+
+# Playing assistant sound function
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\start_sound.mp3"  # Path to your sound file
+    playsound(music_dir)  # Call the playsound function
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
 import eel
 
+from engine.features import *
+
 eel.init("www")
+
+playAssistantSound()
+
 
 # Open Microsoft Edge in app mode
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
 # eel.start() function
 eel.start('index.html', mode=None, host='localhost', block=True)
+
